39f4a2832437de07d6e9a613ee6dcef4b94d7c7c
etrufanov <59361669+etrufanov@users.noreply.github.com>
2024-04-07 11:13:45 +0300

    2 output an image (#2)
    * ray_tracing_in_one_weekend init
    
    * Chapter 2. Output an Image

--- ray_tracing_in_one_weekend/src/main.rs
+++ ray_tracing_in_one_weekend/src/main.rs
@@ -1,3 +1,20 @@
+mod create_img_arr;
+mod image_generators_catalog;
+mod write_img_arr_to_file;
+
+use std::path::Path;
+
+use create_img_arr::create_img_arr;
+use image_generators_catalog::black_and_white_gradient::BlackAndWhiteGradient;
+use write_img_arr_to_file::write_img_arr_to_file;
+
+const IMAGES_DIR: &str = "./src/images";
+
 fn main() {
-    println!("Hello, world!");
+    let black_and_white_gradient_path: &Path = &Path::new(IMAGES_DIR).join("black_and_white_gradient.ppm");
+
+    let black_and_white_gradient = BlackAndWhiteGradient::new(512, 512);
+    let black_and_white_gradient_arr = create_img_arr(&black_and_white_gradient);
+
+    write_img_arr_to_file(black_and_white_gradient_path, black_and_white_gradient_arr);
 }
